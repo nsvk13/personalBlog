f58d4a04bb56141ece8592b8d0bb332e256ebf62
Nikita Simakin <84946011+nsvk13@users.noreply.github.com>
2024-01-02 20:45:34 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
   url: 'https://nsvk13.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/personalBlog',
-
+  baseUrl: '/personalBlog/',
+  deploymentBranch: 'main',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nsvk13', // Usually your GitHub org/user name.
